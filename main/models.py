7d19263b83c4c2e68b80f42bcf3ca45063a753ad
Nico <9906385+ThaNico@users.noreply.github.com>
2022-08-13 16:57:57 +0800

    create model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class HistoricHour(models.Model):
+    class Status(models.IntegerChoices):
+        ON_HOLD = 0, 'On hold'
+        VALID = 1, 'Valid'
+
+    time = models.TimeField()
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(2100),
+            MinValueValidator(1500)
+        ]
+    )
+    label = models.CharField(max_length=128)
+    source = models.URLField()
+    status = models.PositiveSmallIntegerField(choices=Status.choices, default=Status.ON_HOLD)
+
+    def __str__(self):
+        return '{} {} {}'.format(self.time.strftime('%H:%M'), self.year, self.label)
