0d9c2530230e11cee60b47244b2e95d155cb6cba
Nico <9906385+ThaNico@users.noreply.github.com>
2022-08-20 14:36:42 +0800

    rename

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,5 @@ from main.views.HomeView import HomeView
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('events/<str:hours>/<str:minutes>', EventsView.as_view(), name='events'),
-    path('events/add', EventsAddView.as_view(), name='events-add'),
+    path('add-event', EventsAddView.as_view(), name='add-event'),
 ]
